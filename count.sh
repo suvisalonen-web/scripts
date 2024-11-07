956fe7f4580aa50bcf1d7c87da1864e6ed9e1b70
Suvi <suski_311@hotmail.com>
2024-11-07 19:46:24 +0200

    new version

--- count.sh
+++ count.sh
@@ -2,19 +2,19 @@
 
 
 # step 1
-echo "current directory"
-pwd
+#echo "current directory"
+#pwd
 
 #step 2
-echo "changing to /bin"
+echo "Changing to /bin"
 cd /bin
-echo "number of files: `ls -lrt | wc -l`"
+echo "Number of files: `ls -lrt | wc -l`"
 
 #step 3
-echo "changing to /var"
+echo "Changing to /var"
 cd /var
-echo "number of files: `ls -lrt | wc -l`"
+echo "Number of files: `ls -lrt | wc -l`"
 
 
 echo "Author: Suvi"
-echo "Version: 1.0"
+echo "Version: 1.1"
