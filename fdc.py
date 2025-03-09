2e669df85ae6c8341f2cc618fa10370e3d35da24
suvisalonen-web <suski_311@hotmail.com>
2025-03-09 17:17:33 +0200

    version 4

--- fdc.py
+++ fdc.py
@@ -5,23 +5,31 @@
 import os
 import time
 
+
+def fdirCount(path):
+
+    files, dirs= 0, 0
+    for root, dirnames, filenames in os.walk(path):
+        dirs+= len(dirnames)
+        files += len(filenames)
+
+    # return the variables dirs and files as a tuple
+    return (files, dirs)     
+
 print('Type your directory')
 
 path=input()
+
 start=time.time()
-
-files, dirs= 0, 0
-for root, dirnames, filenames in os.walk(path):
-    dirs+= len(dirnames)
-    files += len(filenames)
-
+files,dirs=fdirCount(path)
 end=time.time()
 elapsed=end-start
+
 
 # Grand Total
 print('Files:', files)
 print('Directories:', dirs)
-print('Elapsed time (sec)',elapsed)
+print('Elapsed time:',elapsed, '(sec)')
 
 
 
