a9c9a430aded375c0e4b2ca2407bc13499b69873
suvisalonen-web <suski_311@hotmail.com>
2025-03-09 16:30:15 +0200

    fdc name

---
+++ fdc.py
@@ -0,0 +1,28 @@
+
+# fdc - files directory count
+
+import os
+import time
+
+print('Type your directory')
+
+path=input()
+start=time.time()
+
+files, dirs= 0, 0
+for root, dirnames, filenames in os.walk(path):
+    dirs+= len(dirnames)
+    files += len(filenames)
+
+end=time.time()
+elapsed=end-start
+
+# Grand Total
+print('Files:', files)
+print('Directories:', dirs)
+print('Elapsed time (sec)',elapsed)
+
+
+
+
+    
